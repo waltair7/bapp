5958ed4f62afd3ef619818c9b9c46f4855ec16a8
dvsuresh <dv.mymail@gmail.com>
2013-02-23 23:56:42 -0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bapp::Application.config.secret_token = '7877cb2325f8bc22d63f69de4d73ea568ecfd0e57ea4c7e223ba1cf1f5effb0943bc444ed0a0b0e61b675f4b1b5f74813e51fb422e2536f6e3ff495b84c92a30'
